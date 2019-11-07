8ec8df4f8ed23225b701c0a32162e2e3eedaae81
潘景然 <18612892806@163.com>
2019-11-07 14:51:22 +0800

    修改eureka调用

--- src/main/java/com/leyou/userserrvice/UserSerrviceDemoApplication.java
+++ src/main/java/com/leyou/userserrvice/UserSerrviceDemoApplication.java
@@ -3,10 +3,11 @@ package com.leyou.userserrvice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserSerrviceDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserSerrviceDemoApplication.class, args);
--- src/main/java/com/leyou/userserrvice/controller/UserController.java
+++ src/main/java/com/leyou/userserrvice/controller/UserController.java
@@ -3,15 +3,12 @@ package com.leyou.userserrvice.controller;
 import com.leyou.userserrvice.pojo.User;
 import com.leyou.userserrvice.service.UserSerrvice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.awt.geom.AreaOp;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -25,14 +22,10 @@ public class UserController {
     private final Logger logger=Logger.getLogger("UserController");
     @Autowired
     private UserSerrvice userSerrvice;
-    @Autowired
-    private DiscoveryClient client;
+
 
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id){
-        List<ServiceInstance> list = client.getInstances("user");
-        ServiceInstance instance = list.get(0);
-        logger.info("serviceId"+instance.getServiceId()+"host:post="+instance.getHost()+":"+instance.getPort());
         return this.userSerrvice.queryById(id);
     }
 
